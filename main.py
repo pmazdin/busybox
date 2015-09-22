783135302797360828b812fd6edbad50c25bd0b4
pmazdin <petra.mazdin1@gmail.com>
2015-09-22 10:28:15 +0200

    main script to run collect_data to do time stamp analysis

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+'''
+	python file to run all functions needed to analyze 
+	data from busybox ash
+'''
+
+import collect_data as cd
+
+cd.loadScores()
+
+cd.calculateDuration()
+
